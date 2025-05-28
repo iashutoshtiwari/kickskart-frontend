d73dc070ea1afe8a7088f57ba4116771ab9f3cb3
Ashutosh Tiwari <mail@ashutoshtiwari.co.in>
2025-05-28 15:17:22 +0530

    Updated to new endpoint

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,7 +1,7 @@
 export const getApiUrl = () =>
-	//TODO: Modify this method to support environment variables
-	"https://sneaker-store-alpha.cyclic.app";
+  //TODO: Modify this method to support environment variables
+  'https://kickskart-backend.onrender.com'
 
 export const getAuthToken = () => {
-	return localStorage.getItem("token");
-};
+  return localStorage.getItem('token')
+}
